f1060d0fffc7f6311ac37247ec5259b4dbc309be
Bekam Alemayehu <108836106+bekamalemayehu@users.noreply.github.com>
2023-11-29 16:59:15 -0500

    Add files via upload

---
+++ TimeConverter.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import info
+
+def start():
+    st.title(info.title)
+    st.write("I can convert between 12 hour time and 24 hour time!")
+    st.write("---")
+start()
+name = st.text_input("What is your first name?") #NEW
+
+
+def function():
+    #name = st.text_input("What is your first name?")
+    timeType = st.radio(f"{name}, do you want to convert your time to 12 or 24 hour time?",["24-hour time","12-hour time"])#NEW
+    if timeType == "24-hour time":
+        st.image(info.image24,width = 500)
+        hour = st.number_input("Select the hour(s):",min_value=1,max_value=12,value=6,step=1) #NEW
+        minute = st.number_input("Select the minute(s):",min_value=0,max_value=59,value=0,step=1)
+        amPm = st.radio("Is that time in AM or PM?",["a.m.","p.m."])
+        newmin = ""
+        
+        if minute < 10:
+            newmin = f"0{minute}"
+        else:
+            newmin = minute
+
+        if amPm != "p.m.":
+            newhour = hour
+            st.write(f"{name}, your time in 12 hour time to 24 hour time is **{newhour}:{newmin}.**")
+        else:
+            if hour == 12:
+                newhour = hour
+            else:
+                newhour = hour + 12
+            st.write(f"{name}, your time in 12 hour time to 24 hour time is **{newhour}:{newmin}.**")
+    else:
+        st.image(info.image12, width = 500)
+        hour = st.number_input("Select the hour(s):",min_value=0,max_value=23,value=12,step=1)
+        minute = st.number_input("Select the minute(s):",min_value=0,max_value=59,value=0,step=1)
+        newmin = ""
+        if minute < 10:
+            newmin = f"0{minute}"
+        else:
+            newmin = minute
+        
+        if hour < 13 and hour > 0:
+            newhour = hour
+            st.write(f"{name}, your time in 24 hour time to 12 hour time is **{newhour}:{newmin} a.m.**")
+        elif hour == 0:
+            st.write(f"{name}, your time in 24 hour time to 12 hour time is **12:{newmin} a.m.**")
+        else:
+            newhour = hour-12
+            st.write(f"{name}, your time in 24 hour time to 12 hour time is **{newhour}:{newmin} p.m.**")
+
+    
+if name != "":
+    function()
---
+++ info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/Rambling Reck.jpg"
+about_me = "I'm Bekam Alemayehu. I am a student at Georgia Tech and love coding. Go Jackets!"
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/bekam-alemayehu-939784152/"
+my_github_url = "https://github.com/bekamalemayehu"
+my_email_address = "bekamalemayehu8@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1554",], 
+    "names":["Intro to CS", "Linear Algebra"], 
+    "semester_taken":["1st", "1st"],
+    "skills":["Python", "Row reduction, matrix, Google PageRank"],
+    }
+experience_data = {
+    "Hardlines Team Associate": (["- Assisted customers with finding items, mixing paint, registering for hunting licenses.",
+                                "- Collaborated with the Team Lead and other Team Associates with setting up event displays and completing freight. ", "- Earned the award of Sales Floor Associate of the Month within 1 month of working. "],"images/Walmart.jpg")}
+
+projects_data = {
+    "Time Converter Website": "Created a website that lets you convert between 24-hour time and 12-hour time.",
+    "Building Interactive Websites": "Created an interactive form with 6 questions to recommend games instead of the minimum 5 questions. Use JavaSript, HTML, CSS, and Visual Studio Code.",
+}
+
+programming_data = {
+    "Python": 100,
+    "Java": 5
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕"
+}
+spoken_icons = {
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸",
+    "Afaan Oromo": "🇪🇹"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Beginner",
+    "Afaan Oromo": "Native"
+}
+leadership_data = {
+    "Associate at Walmart": (["- Earned the award of Sales Floor Associate of the Month"],"images/employee.jpg"),
+
+}
+activity_data={
+    "Clarkston High School Gardening Club": ["- Provided care for community school gardens for 16.5 hours.", 
+            "- Planted, weeded, watered, and harvested for community distribution."]
+}
+
+title = "12 Hour and 24 Hour Time Converter"
+image12 = "images/12-hour.jpg"
+image24 = "images/24-hour.jpg"
